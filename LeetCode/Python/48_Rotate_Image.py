b37647f9fb6502bc92d0ae906a69267d91a0e15e
Francisco del Campo <frankiedelcampo@rocketmail.com>
2023-10-28 00:23:45 -0400

    Add 48

---
+++ LeetCode/Python/48_Rotate_Image.py
@@ -0,0 +1,12 @@
+class Solution:
+    # The ith row becomes the (n-i)th column
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for row in range(n):
+            matrix[row] = matrix[row][::-1]
