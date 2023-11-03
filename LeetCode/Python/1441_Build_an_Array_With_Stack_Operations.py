278f3dc3a41975411a88a81c004bb8000396e54c
Francisco del Campo <frankiedelcampo@rocketmail.com>
2023-11-03 09:32:28 -0400

    Add 1441

---
+++ LeetCode/Python/1441_Build_an_Array_With_Stack_Operations.py
@@ -0,0 +1,14 @@
+xclass Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        idx = 0
+        res = []
+        for i in range(1, n+1):
+            if idx >= len(target):
+                break
+            if i == target[idx]:
+                res.append('Push')
+                idx += 1
+            else:
+                res.append('Push')
+                res.append('Pop')
+        return res
