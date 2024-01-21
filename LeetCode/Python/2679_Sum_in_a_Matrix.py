7490afd12fe685c25ddb645e71ec6b2b4a7d4bcf
Francisco del Campo <frankiedelcampo@rocketmail.com>
2024-01-21 01:41:59 -0500

    Fix 2679

--- LeetCode/Python/2679_Sum_in_a_Matrix.py
+++ LeetCode/Python/2679_Sum_in_a_Matrix.py
@@ -1,55 +1,13 @@
 class Solution:
-    def minDifference(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n == 4:
-            return 0
-        low_1 = float('inf')
-        low_2 = float('inf')
-        low_3 = float('inf')
-        low_4 = float('inf')
-        high_1 = float('-inf')
-        high_2 = float('-inf')
-        high_3 = float('-inf')
-        high_4 = float('-inf')
-        for num in nums:
-            if num <= low_1:
-                low_4 = low_3
-                low_3 = low_2
-                low_2 = low_1
-                low_1 = num
-            elif num <= low_2:
-                low_4 = low_3
-                low_3 = low_2
-                low_2 = num
-            elif num <= low_3:
-                low_4 = low_3
-                low_3 = num
-            elif num <= low_4:
-                low_4 = num
-
-            if num >= high_1:
-                high_4 = high_3
-                high_3 = high_2
-                high_2 = high_1
-                high_1 = num
-            elif num >= high_2:
-                high_4 = high_3
-                high_3 = high_2
-                high_2 = num
-            elif num >= high_3:
-                high_4 = high_3
-                high_3 = num
-            elif num >= high_4:
-                high_4 = num
-        res = []
-        if high_4 - low_1 > 0:
-            res.append(high_4 - low_1)
-        if high_3 - low_2 > 0:
-            res.append(high_3 - low_2)
-        if high_2 - low_3 > 0:
-            res.append(high_2 - low_3)
-        if high_1 - low_4 > 0:
-            res.append(high_1 - low_4)
-        if res:
-            return min(res)
-        return 0
+    def matrixSum(self, nums: List[List[int]]) -> int:
+        rows = len(nums)
+        cols = len(nums[0])
+        for i in range(rows):
+            nums[i].sort()
+        res = 0
+        for j in range(cols):
+            m = 0
+            for i in range(rows):
+                m = max(m, nums[i][j])
+            res += m
+        return res
