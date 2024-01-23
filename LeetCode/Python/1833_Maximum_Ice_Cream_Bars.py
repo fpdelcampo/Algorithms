fec54ba191915590e261d354b2a7d3a684716a59
Francisco del Campo <frankiedelcampo@rocketmail.com>
2024-01-23 18:11:00 -0500

    Add 1833

---
+++ LeetCode/Python/1833_Maximum_Ice_Cream_Bars.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+        res = 0
+        for cost in costs:
+            if cost <= coins:
+                coins -= cost
+                res += 1
+        return res
