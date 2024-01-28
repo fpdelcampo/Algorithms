9b762ef0e79d1fec3bc6a2aadfe883ab4dca66a5
Francisco <frankiedelcampo@rocketmail.com>
2024-01-27 23:01:11 -0500

    Almost done

--- Python/Segtree.py
+++ Python/Segtree.py
@@ -1,3 +1,4 @@
+import random
 class Segtree():
     def __init__(self, arr, f = lambda x, y: x + y, identity = 0) -> None:
         self.arr = arr
@@ -14,31 +15,45 @@ class Segtree():
             self.build(2 * v, tl, tm)
             self.build(2 * v + 1, tm + 1, tr)
             self.segtree[v] = self.f(self.segtree[2 * v], self.segtree[2 * v + 1])
+
+    def update_helper(self, v, tl, tr, index, value):
+        if tl == tr == index:
+            self.segtree[v] = value
+        else:
+            tm = (tl + tr) // 2
+            if index <= tm:
+                self.update_helper(2 * v, tl, tm, index, value)
+            else:
+                self.update_helper(2 * v + 1, tm + 1, tr, index, value)
     
     def update(self, index, value):
-        pass
+        self.update_helper(1, 0, len(self.arr) - 1 , index, value)
 
     def query_helper(self, v, tl, tr, l , r):
-        if tl > tr:
+        if l > r:
             return self.identity
         
         if l == tl and r == tr:
             return self.segtree[v]
         
         tm = (tl + tr) // 2
-        print(tl, tm, tr, l, r)
-        return self.f(self.query_helper(2 * v, tl, tm, l, min(r, tm)), self.query_helper(2 * v + 1, tm + 1, tr, max(l, tm + 1), r))
+        return self.f(self.query_helper(2 * v, tl, tm, l, min(tm, r)), self.query_helper(2 * v + 1, tm + 1, tr, max(tm + 1, tl), r))
     
     def query(self, l, r):
         return self.query_helper(1, 0, len(self.arr) - 1, l, r)
 
 def main():
-    arr = list(range(8))
+    arr = list(range(1000))
     segtree = Segtree(arr)
-    print(segtree.segtree)
-    print(segtree.f(1, 2))
-    result = segtree.query(0, 5)
-    print(result)
+    successes = 0
+    for _ in range(500):
+        left = random.randint(0, 999)
+        right = random.randint(left, 999)
+        if segtree.query(left, right)  == (right * (right + 1)) // 2 - (left * (left - 1)) // 2:
+            print(left, right)
+            successes += 1
+    print(segtree.query(500, 717))
+    print(f"Successes: {successes}; Total: {500}")
 
 if __name__ == "__main__":
     main()
